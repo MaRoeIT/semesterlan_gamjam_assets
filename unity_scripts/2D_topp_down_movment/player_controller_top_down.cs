387de9f4721fde148cfd3838c77431190a9489b5
MaRoeIT <142741541+MaRoeIT@users.noreply.github.com>
2023-11-03 18:43:37 +0100

    2D top down movment script

---
+++ unity_scripts/2D_topp_down_movment/player_controller_top_down.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_controller_top_down : MonoBehaviour
+{
+    // Variabel for å sette hastigheten til karakteren
+    public float character_speed = 5f;
+    // Variabel som holder på elementet sin rigidbody, brukes for å bevege karakteren
+    public Rigidbody2D rigid_body;
+    // Variable for å holde på animatoren til elementet
+    public Animator animator;
+    // Variabel for å holde på x og y aksene
+    Vector2 movment;
+
+    // Update oppdaterer seg basert på frame rate
+    void Update()
+    {
+        // setter y og x aksene til å justeres basert på input.
+        movment.x = Input.GetAxisRaw("Horizontal");
+        movment.y = Input.GetAxisRaw("Vertical");
+        // setter parameter bool til animator kalt idle til true når hastigheten på karakteren er 0
+        if (animator.GetFloat("speed") == 0f )
+        {
+            animator.SetBool("idle", true);
+        }
+        else
+        {
+            animator.SetBool("idle", false);
+        }
+
+        // Setter parameterne i animatoren til å endre basert på bevegelsene til karakteren.
+        animator.SetFloat("horizontal", movment.x);
+        animator.SetFloat("vertical", movment.y);
+        animator.SetFloat("speed", movment.sqrMagnitude);
+        
+    }
+
+    // Fixed update oppdaterer 50 ganger i sekundet og derfor er mer stabilt vis man skal gjøre ting som krever pyshics slik at det ikke varierer basert på frame rate
+    void FixedUpdate()
+    {
+        // bevegelsen for karkateren
+        rigid_body.MovePosition(rigid_body.position + movment * character_speed * Time.fixedDeltaTime);
+    }
+}
